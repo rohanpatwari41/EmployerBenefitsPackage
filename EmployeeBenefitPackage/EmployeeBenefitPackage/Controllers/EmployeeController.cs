56f71767f678a0989a9fa5d736d36a1c580ce0e4
ARCHCAPSERVICES\RPatwari <rpatwari@archcapservices.com>
2022-03-23 15:16:29 -0400

    added code for emp sal calc

---
+++ EmployeeBenefitPackage/EmployeeBenefitPackage/Controllers/EmployeeController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using EmployeeBenefitPackage.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeBenefitPackage.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(EmpDependent emp)
+        {
+            if (emp.Name != null && emp.MaritalStatus != null)
+            {
+                emp.GrossPay = 2000;
+
+                var BenftPerYr = 83.3; //$1000/year(12 months) for each employee
+
+                var TenPercDisc = 0.1;  //$10% discount for an employee or dependent whose name starts with 'A'
+
+
+                //benefits deduction $1000/year.
+
+                if (emp.MaritalStatus == "Single")
+                {
+                    if (emp.Name.StartsWith("A"))
+                    {
+                        emp.Deduction = BenftPerYr - TenPercDisc;
+                    }
+                    else
+                    {
+                        emp.Deduction = BenftPerYr;
+                    }
+
+                    emp.NetSalary = emp.GrossPay - emp.Deduction;
+
+                    HttpContext.Session.SetString("Name", emp.Name);
+                    HttpContext.Session.SetString("Deduction", emp.Deduction.ToString());
+                    HttpContext.Session.SetString("NetSalary", emp.NetSalary.ToString());
+                    return RedirectToAction("EmpSalaryInfo", "Employee");
+
+                }
+                else if (emp.MaritalStatus == "Married" && emp.Children != null && emp.DependentNameStartsWithA != null)
+                {
+                    var DepdntCstPerYr = 41.6; //$500/year(12 months) for each employee & dependent
+
+                    if (emp.Name.StartsWith("A") && Int32.Parse(emp.DependentNameStartsWithA) != 0)
+                    {
+                        if (emp.Children == "N/A")
+                        {
+                            emp.Deduction = BenftPerYr + DepdntCstPerYr - ((Int32.Parse(emp.DependentNameStartsWithA) + 1) * TenPercDisc);
+                        }
+                        else
+                        {
+                            //dependent(children & spouse) deduction $500/year.
+                            emp.Deduction = BenftPerYr + (DepdntCstPerYr * (Int32.Parse(emp.Children) + 1)) - ((Int32.Parse(emp.DependentNameStartsWithA) + 1) * TenPercDisc);
+                        }
+                    }
+                    else if (emp.Name.StartsWith("A") && Int32.Parse(emp.DependentNameStartsWithA) == 0)
+                    {
+                        if (emp.Children == "N/A")
+                        {
+                            emp.Deduction = BenftPerYr + DepdntCstPerYr - TenPercDisc;
+                        }
+                        else
+                        {
+                            //dependent(children & spouse) deduction $500/year.
+                            emp.Deduction = BenftPerYr + (DepdntCstPerYr * (Int32.Parse(emp.Children) + 1)) - TenPercDisc;
+                        }
+
+                    }
+                    else if (!emp.Name.StartsWith("A") && Int32.Parse(emp.DependentNameStartsWithA) != 0)
+                    {
+                        if (emp.Children == "N/A")
+                        {
+                            emp.Deduction = BenftPerYr + DepdntCstPerYr - (Int32.Parse(emp.DependentNameStartsWithA) * TenPercDisc);
+                        }
+                        else
+                        {
+                            //dependent(children & spouse) deduction $500/year.
+                            emp.Deduction = BenftPerYr + (DepdntCstPerYr * (Int32.Parse(emp.Children) + 1)) - (Int32.Parse(emp.DependentNameStartsWithA) * TenPercDisc);
+                        }
+
+                    }
+                    else
+                    {
+                        if (emp.Children == "N/A")
+                        {
+                            emp.Deduction = BenftPerYr + DepdntCstPerYr;
+                        }
+                        else
+                        {
+                            //dependent(children & spouse) deduction $500/year.
+                            emp.Deduction = BenftPerYr + (DepdntCstPerYr * (Int32.Parse(emp.Children) + 1));
+                        }
+                    }
+                    emp.NetSalary = emp.GrossPay - emp.Deduction;
+
+                    HttpContext.Session.SetString("Name", emp.Name);
+                    HttpContext.Session.SetString("Deduction", emp.Deduction.ToString());
+                    HttpContext.Session.SetString("NetSalary", emp.NetSalary.ToString());
+                    return RedirectToAction("EmpSalaryInfo", "Employee");
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult EmpSalaryInfo()
+        {
+            ViewBag.Name = HttpContext.Session.GetString("Name");
+            ViewBag.Deduction = HttpContext.Session.GetString("Deduction");
+            ViewBag.NetSalary = HttpContext.Session.GetString("NetSalary");
+
+            return View();
+        }
+    }
+}
---
+++ EmployeeBenefitPackage/EmployeeBenefitPackage/Models/EmpDependent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace EmployeeBenefitPackage.Models
+{
+    public class EmpDependent
+    {
+        [Required(ErrorMessage = "Enter Employer Name !")]
+        [Display(Name = "Employer Name:")]
+        public string Name { get; set; }
+        public double GrossPay { get; set; }
+
+        [Required(ErrorMessage = "Select the Marital Status !")]
+        [Display(Name = "Marital Status:")]
+        public string MaritalStatus { get; set; }
+
+        [Required(ErrorMessage = "Enter the No. of Children !")]
+        [Display(Name = "Children:")]
+        public string Children { get; set; }
+
+        [Display(Name = "Total No. of Dependents whose Name Starts With 'A':")]
+        [Required(ErrorMessage = "Enter the No. of Dependents whose Name Starts With 'A' !")]
+        public string DependentNameStartsWithA { get; set; }
+        public double Deduction { get; set; }
+
+        public double NetSalary { get; set; }
+
+
+    }
+}
